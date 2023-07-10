978a12cec6f2efc929865979e94ed51433ae9297
utsmannn <utsmannn@gmail.com>
2023-07-10 18:30:52 +0700

    fix finalized

--- cleanup-open.sh
+++ cleanup-open.sh
@@ -9,15 +9,15 @@ rm -rf iosApp/Pods
 rm -rf iosApp/iosApp.xcodeproj/project.xcworkspace
 rm -rf iosApp/iosApp.xcodeproj/xcuserdata
 
-echo "> Reopen Android Studio when not running on 5 second.."
+echo -en "> Reopen Android Studio when not running on 5 second.."
 sleep 2
-echo "> Reopen Android Studio when not running on 4 second.."
+echo -en "\r> Reopen Android Studio when not running on 4 second.."
 sleep 2
-echo "> Reopen Android Studio when not running on 3 second.."
+echo -en "\r> Reopen Android Studio when not running on 3 second.."
 sleep 2
-echo "> Reopen Android Studio when not running on 2 second.."
+echo -en "\r> Reopen Android Studio when not running on 2 second.."
 sleep 2
-echo "> Reopen Android Studio when not running on 1 second.."
+echo -en "\r> Reopen Android Studio when not running on 1 second.."
 sleep 2
 
 open -a /Applications/Android\ Studio.app
